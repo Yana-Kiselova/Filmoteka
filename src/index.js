53407b1cf1709de0bbdf6dec9865ac621f1eddef
Yana Kiselova <y.kiseleva1602@gmail.com>
2023-08-03 00:56:01 +0300

    added css and made it responsive

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
 import debounce from 'lodash.debounce';
-import templatesCard from './templates/film-card.hbs';
 import templates from './templates/appenFilmMarkup.hbs';
+import templatesModal from './templates/modal.hbs';
 import NewApiService from './js/news-service';
 
+const apiService = new NewApiService();
+
+/////// рендер популярных фильмов //////
+function fetchTrending() {
+  apiService.fetchArticles().then(data => {
+    addListTemplates(data);
+  });
+}
+fetchTrending();
+//////////////////////////////////////////////
 //получаю доступ у элементам//
 const inputEl = document.querySelector('#header-input');
 const galeryEl = document.querySelector('.gallery-list');
-// coздаю новый класс//
-const apiService = new NewApiService();
-apiService.filmRequest();
 
 inputEl.addEventListener('input', debounce(filmName, 1000));
 //если инпут пустая строка =>галерея пустая и выходим
 function filmName(e) {
   if (e.target.value.trim() === '') {
     galeryEl.innerHTML = '';
+    fetchTrending();
     return;
   }
   //если длина массива =0 =>алерт и выход
@@ -23,6 +31,7 @@ function filmName(e) {
     if (data.length === 0) {
       return alert('проверте правильность ввода');
     }
+    galeryEl.innerHTML = '';
     addListTemplates(data);
   });
 }
@@ -31,12 +40,50 @@ function addListTemplates(results) {
   console.log(results, galeryEl);
   galeryEl.insertAdjacentHTML('beforeend', templates(results));
 }
-// рендер 1й карточки//
-const galleryItem = document.querySelector('.gallery-item');
 
-// galleryItem.addEventListener('clic', onModalTeamOpen);
-// Модальное окно для полноразмерного изображения
-// Для того чтобы открыть, необходимо добавить на gallery-item CSS-класс is-open
-// function onModalTeamOpen() {
-//   sectionEl.insertAdjacentHTML('beforeend', templatesCard(results));
-// }
+///////////////////////////modal////////////////////////////
+const refs = {
+  galleryList: document.querySelector('.gallery-list'),
+  galleryItem: document.querySelector('.gallery-item'),
+  backdrop: document.querySelector('.backdrop'),
+  buttonModalClose: document.querySelector('.modal-close'),
+  modalContent: document.querySelector('.modal-content'),
+};
+
+refs.galleryList.addEventListener('click', evt => {
+  openModalFilm(evt);
+});
+
+/////// открыть модолку при клике на img вешаем класс/////
+function openModalFilm(evt) {
+  if (!evt.target.nodeName === 'IMG') {
+    return;
+  }
+  getMovieId(evt);
+  refs.backdrop.classList.add('is-open');
+  refs.buttonModalClose.addEventListener('click', closeModalFilm);
+}
+
+function closeModalFilm(evt) {
+  refs.backdrop.classList.remove('is-open');
+  refs.buttonModalClose.removeEventListener('click', closeModalFilm);
+  refs.modalContent.innerHTML = '';
+}
+// //////////рендер  модалки////
+function getMovieId(evt) {
+  const movieId = evt.target.getAttribute('data-movie-id');
+  console.log(movieId);
+  // зaрос данных фильма по ID
+  apiService
+    .getFilmById(movieId)
+    .then(movie => {
+      console.log(movie);
+      // TODO: Add template for render movie in modal
+      // TODO: Insert template in modal html
+
+      refs.modalContent.insertAdjacentHTML('beforeend', templatesModal(movie));
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
--- src/js/news-service.js
+++ src/js/news-service.js
@@ -6,16 +6,34 @@ export default class NewApiService {
     this.searchQuery = '';
     this.page = 1;
   }
+  // запрос за популярными фильмами при загрузке сайта ////
+  fetchArticles() {
+    const url = `https://api.themoviedb.org/3/trending/movie/week?api_key=b32f977d148061c9ab22a471ff2c7792&page=${this.page}`;
+    return fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        this.incrementPage();
+        return data.results;
+      });
+  }
+
+  // запрос за фильмами в поиске ////
   filmRequest() {
     const url = `${BASE_URL}search/movie?api_key=${API_KEY}&language=en-US&page=${this.page}&include_adult=false&query=${this.searchQuery}`;
     return fetch(url)
       .then(response => response.json())
       .then(data => {
-        console.log(data);
         this.incrementPage();
         return data.results;
       });
   }
+
+  // запрос за фильмо по Id ////
+  getFilmById(id) {
+    const url = `${BASE_URL}movie/${id}?api_key=${API_KEY}&language=en-US`;
+    return fetch(url).then(response => response.json());
+  }
+
   incrementPage() {
     this.page += 1;
   }
