06a120441b57744168207580e27e25c5089675e6
Yana Kiselova <y.kiseleva1602@gmail.com>
2023-08-06 23:36:58 +0300

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -2574,13 +2574,9 @@ function closeModalFilm(evt) {
 // //////////рендер  модалки////
 function getMovieId(evt) {
   movieId = evt.target.getAttribute('data-movie-id');
-  console.log(movieId);
   // зaрос данных фильма по ID
   apiService.getFilmById(movieId).then(movie => {
-    console.log(movie);
-    // TODO: Add template for render movie in modal
-    // TODO: Insert template in modal html
-
+    console.log(movieId, movie);
     refs.modalContent.insertAdjacentHTML('beforeend', (0, _modal.default)(movie));
     setButtonsListeners();
   }).catch(err => {
@@ -2588,34 +2584,18 @@ function getMovieId(evt) {
   });
 }
 
-//////coхранение в localStorage/////////
+// ======== coхранение в localStorage ========
+// ======== ищем кнопки и вешаем слушатели на них ========
 function setButtonsListeners() {
   refs.buttonWatched = document.querySelector('.button-watched-js');
   refs.buttonQueue = document.querySelector('.button-queue-js');
   refs.buttonWatched.addEventListener('click', addWatched);
   refs.buttonQueue.addEventListener('click', addQueue);
+  // ======== проверяем фильм в списках и добавляем класс active на кнопку ========
   checkLocalStorage();
 }
-function addWatched(evt) {
-  evt.preventDefault();
-  const watched = (0, _localStorage.getLocalStorage)('watched');
-  if (watched) {
-    if (watched.includes(movieId)) {
-      const arr = watched.filter(id => id !== movieId);
-      (0, _localStorage.setLocalStorage)('watched', arr);
-      refs.buttonWatched.classList.remove('active');
-      refs.buttonWatched.blur();
-      return;
-    }
-    watched.push(movieId);
-    (0, _localStorage.setLocalStorage)('watched', watched);
-    refs.buttonWatched.classList.add('active');
-  } else {
-    const arr = [];
-    arr.push(movieId);
-    (0, _localStorage.setLocalStorage)('watched', arr);
-  }
-}
+
+// ======== проверяем фильм в списках и добавляем класс active на кнопку ========
 function checkLocalStorage() {
   const watched = (0, _localStorage.getLocalStorage)('watched');
   if (watched && watched.includes(movieId)) {
@@ -2626,25 +2606,84 @@ function checkLocalStorage() {
     refs.buttonQueue.classList.add('active');
   }
 }
+
+// ======== при нажатии на buttonWatched добавляем фильи в watched ========
+function addWatched(evt) {
+  evt.preventDefault();
+  // ======== получаем список фильмов watched из localStorage или null ========
+  const watched = (0, _localStorage.getLocalStorage)('watched');
+  if (watched) {
+    if (watched.includes(movieId)) {
+      // ======== удаляем фильм из watched в locatStorage, если он уже есть ========
+      removeFromLocalStorage('watched', watched, refs.buttonWatched);
+      return;
+    }
+    // ======== добавляем фильм в watched в localStorage ========
+    watched.push(movieId);
+    (0, _localStorage.setLocalStorage)('watched', watched);
+    // ======== вешаем класс active на кпопку buttonWatched ========
+    refs.buttonWatched.classList.add('active');
+  } else {
+    // ======== создаем новый массив и записываем фильм в watched в localStorage ========
+    addToLocalStorage('watched');
+  }
+  // ======== удаляем фильм в Queue из localStorage, если такой фильм есть ========
+  removeFromQueue();
+}
+
+// ======== при нажатии на buttonQueue добавляем фильмы в queue ========
 function addQueue(evt) {
   evt.preventDefault();
+  // ======== получаем список фильмов queue из localStorage или null ========
   const queue = (0, _localStorage.getLocalStorage)('queue');
   if (queue) {
     if (queue.includes(movieId)) {
-      const arr = queue.filter(id => id !== movieId);
-      (0, _localStorage.setLocalStorage)('queue', arr);
-      refs.buttonQueue.classList.remove('active');
-      refs.buttonQueue.blur();
+      // ======== удаляем фильм из queue в locatStorage, если он уже есть ========
+      removeFromLocalStorage('queue', queue, refs.buttonQueue);
       return;
     }
+    // ======== добавляем фильм в queue в localStorage ========
     queue.push(movieId);
     (0, _localStorage.setLocalStorage)('queue', queue);
+    // ======== вешаем класс active на кпопку buttonQueue ========
     refs.buttonQueue.classList.add('active');
   } else {
-    const arr = [];
-    arr.push(movieId);
-    (0, _localStorage.setLocalStorage)('queue', arr);
+    // ======== создаем новый массив и записываем фильм в queue в localStorage ========
+    addToLocalStorage('queue');
   }
+  // ======== удаляем фильм в Watched из localStorage, если такой фильм есть ========
+  removeFromWatched();
+}
+
+// ======== при нажатии на buttonWatched удаляем фильм в queue ========
+function removeFromQueue() {
+  const queue = (0, _localStorage.getLocalStorage)('queue');
+  if (queue && queue.includes(movieId)) {
+    removeFromLocalStorage('queue', queue, refs.buttonQueue);
+  }
+}
+
+// ======== при нажатии на buttonQueue удаляем фильм в watched ========
+function removeFromWatched() {
+  const watched = (0, _localStorage.getLocalStorage)('watched');
+  if (watched && watched.includes(movieId)) {
+    removeFromLocalStorage('watched', watched, refs.buttonWatched);
+  }
+}
+
+//======== функция добавления нового массива в список по ключу ========
+function addToLocalStorage(key) {
+  const arr = [];
+  arr.push(movieId);
+  (0, _localStorage.setLocalStorage)(key, arr);
+}
+
+// ======== функция удаление фильма из localStorage ========
+function removeFromLocalStorage(key, array, button) {
+  const newArr = array.filter(id => id !== movieId);
+  (0, _localStorage.setLocalStorage)(key, newArr);
+  button.classList.remove('active');
+  button.blur();
 }
 },{"./news-service":"js/news-service.js","../templates/modal.hbs":"templates/modal.hbs","./local-storage":"js/local-storage.js"}],"index.js":[function(require,module,exports) {
 "use strict";
@@ -2716,7 +2755,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49823" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49791" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
