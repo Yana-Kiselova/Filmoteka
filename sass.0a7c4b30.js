a518266b508979ef68ac1fc3d3cb31ddff79db7f
Yana Kiselova <y.kiseleva1602@gmail.com>
2023-08-23 00:36:57 +0300

    Updates

--- sass.0a7c4b30.js
+++ sass.0a7c4b30.js
@@ -201,7 +201,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60826" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56950" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
